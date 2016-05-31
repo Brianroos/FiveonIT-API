4aba3fd1ba35121bcf9e4dc400ea2c9abd3c33f8
Jeroen Knoester <jeroenknoester@gmail.com>
2016-05-31 12:49:17 +0200

    added newline
    for purposes

--- index.php
+++ index.php
@@ -3,17 +3,17 @@
     header('Content-Type: application/json');
 
     $array = array();
-            
+
     if(isset($_GET['id']) && $_GET['id'] == 1) {
         // Accounts
         $query = 'SELECT * FROM fiveonit ORDER BY id ASC';
         $result = mysql_query($query, $conn);
         $arrayAcc = array();
-        
+
         while($item = mysql_fetch_assoc($result)) {
             $arrayAcc[] = $item;
         }
-        
+
         $array = array('accounts' => $arrayAcc);
     }
     else if(isset($_GET['id']) && $_GET['id'] == 2) {
@@ -22,7 +22,7 @@
         $resultR = mysql_query($queryR, $conn);
         $arrayRooms = array();
         $countRooms = 0;
-        
+
         while($itemR = mysql_fetch_assoc($resultR)) {
             $arrayRooms[] = $itemR;
 
@@ -31,7 +31,7 @@
             }
         }
 
-        $arrayHotel = array('roomsAvailable' => $countRooms, 
+        $arrayHotel = array('roomsAvailable' => $countRooms,
                             'rooms' => $arrayRooms);
         $array = array('hotel' => $arrayHotel);
     }
@@ -57,9 +57,9 @@
             }
         }
 
-        $arrayHotel = array('roomsAvailable' => $countRooms, 
+        $arrayHotel = array('roomsAvailable' => $countRooms,
                             'rooms' => $arrayRooms);
-        $array = array('accounts' => $arrayAcc, 
+        $array = array('accounts' => $arrayAcc,
                    'hotel' => $arrayHotel);
     }
 
